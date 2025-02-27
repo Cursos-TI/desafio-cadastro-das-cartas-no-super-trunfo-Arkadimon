edcf12ebf1390cf9d23c3076e4dcb909a57b34c6
Arkadimon <161147590+Arkadimon@users.noreply.github.com>
2025-02-26 22:47:26 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,13 +19,13 @@ int main() {
     * inserindo alguma coisa no lugar errado ou esquecendo coisas importantes para trás.
     */
    
-    printf("Digite o estado (Escolha uma letra entre 'A' e 'H'): \n"); //Essa função define como a solicitação vai aparecer para o usuário no terminal.
-    scanf("%s", &estado); //Essa função vai ler a resposta do usuário à solicitação feita.
+    printf("Digite o Estado (Escolha uma letra entre 'A' e 'H'): \n"); //Essa função define como a solicitação vai aparecer para o usuário no terminal.
+    scanf(" %c", &estado); //Essa função vai ler a resposta do usuário à solicitação feita.
 
     printf("Digite o código da carta (Uma letra acompanhada de dois números, ex.: 'A05'): \n");
     scanf("%s", &codigoCarta);
 
-    printf("Digite o nome da cidade (Ex.: 'Rio de Janeiro'): \n");
+    printf("Digite o nome da cidade: \n");
     scanf("%s", &nomeCidade);
 
     printf("Digite a quantidade de pessoas que moram na cidade (Pode ser um número aproximado): \n");
@@ -43,12 +43,12 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Aqui iremos definir como aparecerão as informações após o usuário digitar e enviar cada uma delas.
     
-    printf("Estado: %s\n", estado); //Essa é a função que irá imprimir as informações no terminal.
+    printf("Estado: %c\n", estado); //Essa é a função que irá imprimir as informações no terminal.
     printf("Código da carta: %s\n", codigoCarta);
     printf("Cidade: %s\n", nomeCidade);
     printf("População: %d\n", população);
-    printf("Área: %f\n", area);
-    printf("PIB: %f\n", pib);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
     printf("Número de Pontos Turísticos: %d\n", numeroPontosTuristicos);
 
     return 0;
